4b49d8b13cc8859608148fd58c2ad1074db5750c
Marcellu-s <115178042+Marcellu-s@users.noreply.github.com>
2022-12-19 16:13:46 -0300

    Atualização

--- Criando um Sistema Bancário com Python/main.py
+++ Criando um Sistema Bancário com Python/main.py
@@ -2,7 +2,7 @@ from modulos import *
 from os import system
 
 
-conta = {'saldo': 0, 'valor_sacado': [], 'valor_depositado': []}
+conta = {'saldo': 0, 'extrato': []}
 numero_saque = 0
 LIMITE_SAQUE = 3
 
@@ -11,22 +11,23 @@ while True:
     system('cls')
 
     opcao = menu('BANCO DIO', ['Depositar', 'Sacar', 'Extrato', 'Sair'])
-
+    
+    system('cls')
+    
     match opcao:
         case 1:
-            system('cls')
             conta = depositar(conta)
-            system('pause')
         case 2:
-            system('cls')
+
             if numero_saque != LIMITE_SAQUE:
                 conta = sacar(conta)
                 numero_saque += 1
             else:
                 print(colors('LIMITE DE SAQUE ATINGIDO', 'purple'))
-            system('pause')
         case 3:
             extrato(conta)
         case 4:
             print(colors('\nVOLTE SEMPRE...\n', 'green'))
             break
+
+    system('pause')
